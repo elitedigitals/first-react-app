2e5e7eeda24abe7a9480759ac9a8f4409d93037a
elitedigitals <104247277+elitedigitals@users.noreply.github.com>
2025-03-30 21:13:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-        <p>
+        <h1>
           Hello World
-        </p>
+        </h1>
       </header>
     </div>
   );
